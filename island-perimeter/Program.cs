73797476dc552b9b8da10d8e9d3ff689c0b6903f
Nicolas Wolf <haiyang@awolf.net>
2021-10-15 01:40:07 +0800

    optimize

--- island-perimeter/Program.cs
+++ island-perimeter/Program.cs
@@ -20,6 +20,7 @@ namespace island_perimeter
             grid[3] = new int[4] { 1, 1, 0, 0 };
 
             Console.WriteLine(IslandPerimeter(grid));
+            Console.ReadKey();
         }
 
         static int IslandPerimeter(int[][] grid)
@@ -35,48 +36,16 @@ namespace island_perimeter
                     int v = grid[i][j];
                     if (v == 1)
                     {
-                        int top = 0;
-                        if (i == 0)    //first row, top boundary
-                        {
-                            top = 1;
-                        }
-                        else
-                        {
-                            top = Perimeter(grid[i - 1][j]);
-                        }
+                        int top = Perimeter(grid, i - 1, j, i == 0);
                         total += top;
 
-                        int bottom = 0;
-                        if (i + 1 >= row)    //last row, bottom boundary
-                        {
-                            bottom = 1;
-                        }
-                        else
-                        {
-                            bottom = Perimeter(grid[i + 1][j]);
-                        }
+                        int bottom = Perimeter(grid, i + 1, j, i + 1 >= row);
                         total += bottom;
 
-                        int left = 0;
-                        if (j == 0)    //most left, left boundary
-                        {
-                            left = 1;
-                        }
-                        else
-                        {
-                            left = Perimeter(grid[i][j - 1]);
-                        }
+                        int left = Perimeter(grid, i, j - 1, j == 0);
                         total += left;
 
-                        int right = 0;
-                        if (j + 1 >= col)
-                        {
-                            right = 1;
-                        }
-                        else
-                        {
-                            right = Perimeter(grid[i][j + 1]);
-                        }
+                        int right = Perimeter(grid, i, j + 1, j + 1 >= col);
                         total += right;
 
                     }
@@ -86,15 +55,27 @@ namespace island_perimeter
             return total;
         }
 
-        static int Perimeter(int neighbor)
+        /// <summary>
+        /// Calc cell perimeter 
+        /// </summary>
+        /// <param name="grid">grid</param>
+        /// <param name="row">neighbor cell row</param>
+        /// <param name="col">neighbor cell col</param>
+        /// <param name="boundary">boundar condition</param>
+        /// <returns></returns>
+        static int Perimeter(int[][] grid, int row, int col, bool boundary)
         {
-            if (neighbor == 1)
+            if (boundary)
             {
-                return 0;
+                return 1;
+            }
+            else if (grid[row][col] == 0)  //if neighbor is water
+            {
+                return 1;
             }
             else
             {
-                return 1;
+                return 0;
             }
         }
     }

9daeb0c738364428dfdcfecfb2ee87cbca6a6b04
Nicolas Wolf <haiyang@awolf.net>
2021-10-15 02:20:39 +0800

    resolved 476. Number Complement

--- number-complement/Program.cs
+++ number-complement/Program.cs
@@ -10,6 +10,50 @@ namespace number_complement
     {
         static void Main(string[] args)
         {
+            int num = 5;
+            Console.WriteLine(FindComplement(num));
+            Console.ReadKey();
         }
+
+        static int FindComplement(int num)
+        {
+            byte[] bin = Dec2Bin(num);
+
+            List<byte> list = new List<byte>();
+            foreach (byte b in bin)
+            {
+                list.Add((byte)(b ^ 1));
+            }
+
+            byte[] target = list.ToArray();
+            int r = Bin2Dec(target);
+            return r;
+        }
+        static byte[] Dec2Bin(int num)
+        {
+            List<byte> list = new List<byte>();
+            int mod = 0;
+            while (num >= 1)
+            {
+                mod = num % 2;
+                num = num / 2;
+                list.Add(Convert.ToByte(mod));
+            }
+            list.Reverse();
+            return list.ToArray();
+        }
+
+        static int Bin2Dec(byte[] num)
+        {
+            int power = num.Length;
+            int r = 0;
+            foreach(byte b in num)
+            {
+                power--;
+                r += b * (int)Math.Pow(2, power);
+            }
+            return r;
+        }
+
     }
 }

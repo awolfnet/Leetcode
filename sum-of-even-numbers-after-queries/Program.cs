6a230047839c70de0fa568dfdb0e83beacc80ac6
Nicolas Wolf <haiyang@awolf.net>
2021-10-15 14:30:02 +0800

    add problem 985. Sum of Even Numbers After Queries

---
+++ sum-of-even-numbers-after-queries/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_even_numbers_after_queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

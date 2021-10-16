a7ee02edbba6a948bc0cdf690582bf5fc74cc221
Nicolas Wolf <haiyang@awolf.net>
2021-10-17 00:39:17 +0800

    solved tescase 1

--- candy/Program.cs
+++ candy/Program.cs
@@ -10,6 +10,44 @@ namespace candy
     {
         static void Main(string[] args)
         {
+            int[] ratings = new int[] { 1, 2, 2 };
+
+            Console.WriteLine(Candy(ratings));
+            Console.ReadKey();
+        }
+
+        static int Candy(int[] ratings)
+        {
+            int minimum = ratings.Length;
+            int lastCandy = 1;
+
+            if (ratings[0] > ratings[1])
+            {
+                minimum++;
+                lastCandy++;
+            }
+
+            for (int i = 1; i < ratings.Length; i++)
+            {
+                int prev = ratings[i - 1];
+                //int next = i == ratings.Length - 1 ? 0 : ratings[i + 1];
+                int c = ratings[i];
+
+                if (c > prev)
+                {
+                    minimum += lastCandy;
+                    lastCandy++;
+                }
+                else
+                {
+                    lastCandy = 1;
+                }
+
+                //1,2,5,8,9,6,3
+                //1,2,3,4,5,1,2
+
+            }
+            return minimum;
         }
     }
 }

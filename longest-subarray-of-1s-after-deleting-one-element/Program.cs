b9275f3dc8ad6c47dca589c1dc5ffff87129a7dd
Wolf <zhao.haiyang@fruitsvending.com>
2021-11-07 16:58:33 +0800

    Add problem: 1493. Longest Subarray of 1's After Deleting One Element

---
+++ longest-subarray-of-1s-after-deleting-one-element/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace longest_subarray_of_1s_after_deleting_one_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

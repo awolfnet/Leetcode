e98e837f99858538d0a006cf3e872eafbb88314e
Nicolas Wolf <haiyang@awolf.net>
2022-02-14 23:10:44 +0800

    bug: when only 1 element in array

--- maximum-product-subarray/Program.cs
+++ maximum-product-subarray/Program.cs
@@ -10,6 +10,30 @@ namespace maximum_product_subarray
     {
         static void Main(string[] args)
         {
+            int[] nums = { -2 };
+            Console.WriteLine(MaxProduct(nums));
+            Console.ReadKey();
+        }
+
+        static int MaxProduct(int[] nums)
+        {
+            int l = nums.Length;
+
+            int result = 0;
+
+            for (int i = 0; i < l; i++)
+            {
+                int r = nums[i];
+                for(int j=i+1;j<l;j++)
+                {
+                    r *= nums[j];
+                    result = Math.Max(result, r);
+                }
+                
+            }
+
+
+            return result;
         }
     }
 }

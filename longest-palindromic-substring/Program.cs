41654e6417d8b20d69d8146780153a5250996124
Wolf <haiyang@awolf.net>
2022-04-29 17:09:15 +0800

    print each combine

--- longest-palindromic-substring/Program.cs
+++ longest-palindromic-substring/Program.cs
@@ -10,7 +10,39 @@ namespace longest_palindromic_substring
     {
         static void Main(string[] args)
         {
+            string s = "babad";
+
+            string result = LongestPalindrome(s);
+
             Console.ReadKey();
+        }
+
+        public static string LongestPalindrome(string s)
+        {
+            int l = s.Length;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < l; i++)
+            {
+                for (int j = 0; j < l - i+1; j++)
+                {
+                    Console.WriteLine(s.Substring(i, j));
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Length of a palindrome string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int IsPalindrome(string s)
+        {
+
+            return 2;
         }
     }
 }

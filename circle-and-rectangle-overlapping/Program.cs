fb5c8883d2f6824f9ed49afa50fe3808b93af457
Wolf <haiyang@awolf.net>
2022-06-20 12:01:25 +0800

    add check

--- circle-and-rectangle-overlapping/Program.cs
+++ circle-and-rectangle-overlapping/Program.cs
@@ -29,6 +29,33 @@ namespace circle_and_rectangle_overlapping
 
         public bool CheckOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2)
         {
+            Point pc = new Point(xCenter, yCenter);
+
+            //left
+            Point pc1 = new Point(xCenter - radius, yCenter);
+
+            //right
+            Point pc2 = new Point(xCenter + radius, yCenter);
+
+            //bottom
+            Point pc3 = new Point(xCenter, yCenter - radius);
+
+            //top
+            Point pc4 = new Point(xCenter, yCenter + radius);
+
+            //bottom-left
+            Point p1 = new Point(x1, y1);
+
+            //top-right
+            Point p2 = new Point(x2, y2);
+
+            //top-left
+            Point p1a = new Point(x1, y2);
+
+            //bottom-right
+            Point p2a = new Point(x2, y1);
+
+
 
 
 

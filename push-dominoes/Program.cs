abf212b12b492edd4f336a92f1a627a65053fcc0
Wolf <haiyang@awolf.net>
2022-06-20 16:48:56 +0800

    bug: continuous move

--- push-dominoes/Program.cs
+++ push-dominoes/Program.cs
@@ -10,7 +10,7 @@ namespace push_dominoes
     {
         static void Main(string[] args)
         {
-            string dominoes = ".L.R...LR..L..";
+            string dominoes = ".L.R...LR..L........L";
 
             string result = PushDominoes(dominoes);
             Console.WriteLine(result);
@@ -37,12 +37,190 @@ namespace push_dominoes
                 i++;
             }
 
+            int length = dominoes.Length;
+
+            Console.WriteLine(new string(result));
+            foreach (int a in actions)
+            {
+                Console.Write(a);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+            while (!done(actions))
+            {
+                char[] dmove = new char[result.Length];
+                result.CopyTo(dmove, 0);
+
+                int[] dactions = new int[actions.Length];
+                actions.CopyTo(dactions, 0);
+
+                for (i = 0; i < length; i++)
+                {
+                    int action = actions[i];
+                    int actleft = i >= 1 ? actions[i - 1] : 0;
+                    int actright = i < length - 1 ? actions[i + 1] : 0;
+
+                    if (action == 1 && (actleft != 1 || actright != 1))
+                    {
+
+                        char left = i >= 1 ? result[i - 1] : '.';
+                        char right = i < length - 1 ? result[i + 1] : '.';
+                        char d = move(left, right);
+                        Console.WriteLine($"{i}:{left}_{result[i]}_{right} -- {d}");
+                        dmove[i] = d;
+                        dactions[i] = 0;
+                    }
+                }
+
+                dmove.CopyTo(result, 0);
+                dactions.CopyTo(actions, 0);
+                Console.WriteLine(new string(result));
+                foreach (int a in actions)
+                {
+                    Console.Write(a);
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+
+            //Console.WriteLine(new string(result));
+            //char[] dmove = result;
+            //int[] dactions = actions;
+
+            //for (i = 0; i < length; i++)
+            //{
+            //    int action = actions[i];
+            //    if (action == 1)
+            //    {
+            //        char dRight = '.', dLeft = '.';
+            //        if (i == 0)
+            //        {
+            //            dRight = result[i + 1];
+            //        }
+            //        else if (i == length - 1)
+            //        {
+            //            dLeft = result[i - 1];
+            //        }
+            //        else
+            //        {
+            //            dRight = result[i + 1];
+            //            dLeft = result[i - 1];
+            //        }
+
+            //        if (dLeft == '.' && dRight == 'L')   //..L
+            //        {
+            //            dmove[i] = 'L';
+            //        }
+            //        else if (dLeft == '.' && dRight == 'R') //..R
+            //        {
+
+            //        }
+            //        else if (dLeft == 'L' && dRight == '.') //L..
+            //        {
+
+            //        }
+            //        else if (dLeft == 'R' && dRight == '.') //R..
+            //        {
+            //            dmove[i] = 'R';
+            //        }
+            //        else if (dLeft == 'L' && dRight == 'L')    //L.L
+            //        {
+            //            dmove[i] = 'L';
+            //        }
+            //        else if (dLeft == 'R' && dRight == 'R')    //R.R
+            //        {
+            //            dmove[i] = 'R';
+            //        }
+            //        else if (dLeft == 'R' && dRight == '.')    //L.R
+            //        {
+
+            //        }
+            //        else if (dLeft == 'R' && dRight == '.')    //R.L
+            //        {
+
+            //        }
 
 
+            //        Console.WriteLine(new string(dmove));
+            //        foreach (int a in actions)
+            //        {
+            //            Console.Write(a);
+            //        }
+
+
+            //        dactions[i] = 0;
+
+            //        Console.WriteLine();
+            //        Console.ReadKey();
+            //    }
+
+            //}
+            //result = dmove;
+            //actions = dactions;
 
 
 
             return new string(result);
+        }
+
+        static bool done(int[] action)
+        {
+            int r = 0;
+            foreach (int i in action)
+            {
+                r += i;
+            }
+
+            if (r == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static char move(char left, char right)
+        {
+            char result = '.';
+
+            if (left == '.' && right == 'L')   //..L
+            {
+                result = 'L';
+            }
+            else if (left == '.' && right == 'R') //..R
+            {
+
+            }
+            else if (left == 'L' && right == '.') //L..
+            {
+
+            }
+            else if (left == 'R' && right == '.') //R..
+            {
+                result = 'R';
+            }
+            else if (left == 'L' && right == 'L')    //L.L
+            {
+                result = 'L';
+            }
+            else if (left == 'R' && right == 'R')    //R.R
+            {
+                result = 'R';
+            }
+            else if (left == 'R' && right == '.')    //L.R
+            {
+
+            }
+            else if (left == 'R' && right == '.')    //R.L
+            {
+
+            }
+
+            return result;
         }
     }
 }

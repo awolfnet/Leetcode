5f506f41337a4e5ab2d77f2ab84efa2bd1aad4f2
Wolf <haiyang@awolf.net>
2022-06-20 12:14:42 +0800

    add problem push-dominoes

---
+++ push-dominoes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace push_dominoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dominoes = ".L.R...LR..L..";
+
+            string result = PushDominoes(dominoes);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static string PushDominoes(string dominoes)
+        {
+            char[] result = dominoes.ToCharArray();
+            int[] actions = new int[result.Length];
+
+            int i = 0;
+
+            foreach (char d in dominoes)
+            {
+                if (d == 'L' || d == 'R')
+                {
+                    actions[i] = 0;
+                }
+                else
+                {
+                    actions[i] = 1;
+                }
+                i++;
+            }
+
+
+
+
+
+
+            return new string(result);
+        }
+    }
+}

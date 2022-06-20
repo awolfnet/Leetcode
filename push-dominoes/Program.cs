ebb68051e8a2c2d0ca45209999a8fecc207793df
Wolf <haiyang@awolf.net>
2022-06-20 20:25:33 +0800

    only 1 bug left

--- push-dominoes/Program.cs
+++ push-dominoes/Program.cs
@@ -10,7 +10,7 @@ namespace push_dominoes
     {
         static void Main(string[] args)
         {
-            string dominoes = ".L.R...LR..L........L";
+            string dominoes = ".L.R...LR..L........LR";
 
             string result = PushDominoes(dominoes);
             Console.WriteLine(result);
@@ -44,8 +44,8 @@ namespace push_dominoes
             {
                 Console.Write(a);
             }
-            Console.WriteLine();
 
+            Console.WriteLine();
             Console.WriteLine();
 
             while (!done(actions))
@@ -56,23 +56,89 @@ namespace push_dominoes
                 int[] dactions = new int[actions.Length];
                 actions.CopyTo(dactions, 0);
 
+                //Left to right
                 for (i = 0; i < length; i++)
                 {
-                    int action = actions[i];
-                    int actleft = i >= 1 ? actions[i - 1] : 0;
-                    int actright = i < length - 1 ? actions[i + 1] : 0;
+                    char d = result[i];
 
-                    if (action == 1 && (actleft != 1 || actright != 1))
+                    if (d == 'R')
                     {
-
-                        char left = i >= 1 ? result[i - 1] : '.';
-                        char right = i < length - 1 ? result[i + 1] : '.';
-                        char d = move(left, right);
-                        Console.WriteLine($"{i}:{left}_{result[i]}_{right} -- {d}");
-                        dmove[i] = d;
-                        dactions[i] = 0;
+                        //next
+                        int inext = i + 1;
+                        if (inext < length)
+                        {
+                            int a = actions[inext];
+                            if (a == 1)
+                            {
+                                if (inext + 1 >= length)
+                                {
+                                    dmove[inext] = 'R';
+                                }
+                                else
+                                {
+                                    char dnext = result[inext + 1];
+                                    if(dnext!='L')
+                                    {
+                                        dmove[inext] = 'R';
+                                    }
+                                    
+                                }
+                                dactions[inext] = 0;
+                            }
+                        }
                     }
                 }
+
+                //Right to left
+                for (i = length - 1; i >= 0; i--)
+                {
+                    char d = result[i];
+
+                    if (d == 'L')
+                    {
+                        //next
+                        int inext = i - 1;
+                        if (inext >= 0)
+                        {
+                            int a = actions[inext];
+                            if (a == 1)
+                            {
+                                if (inext - 1 < 0)
+                                {
+                                    dmove[inext] = 'L';
+                                }
+                                else
+                                {
+                                    char dnext = result[inext - 1];
+                                    if (dnext != 'R')
+                                    {
+                                        dmove[inext] = 'L';
+                                    }
+
+                                }
+                                dactions[inext] = 0;
+                            }
+                        }
+
+                    }
+                }
+
+                //for (i = 0; i < length; i++)
+                //{
+                //    int action = actions[i];
+                //    int actleft = i >= 1 ? actions[i - 1] : 0;
+                //    int actright = i < length - 1 ? actions[i + 1] : 0;
+
+                //    if (action == 1 && (actleft != 1 || actright != 1))
+                //    {
+                //        char left = i >= 1 ? result[i - 1] : '.';
+                //        char right = i < length - 1 ? result[i + 1] : '.';
+                //        char d = move(left, right);
+                //        Console.WriteLine($"{i}:{left}_{result[i]}_{right} -- {d}");
+                //        dmove[i] = d;
+                //        dactions[i] = 0;
+                //    }
+                //}
 
                 dmove.CopyTo(result, 0);
                 dactions.CopyTo(actions, 0);
